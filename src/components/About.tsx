dec6ee198746b6f81e60186a31f42f063f61433d
victor_lgns11 <victornievezzz@gmail.com>
2025-12-02 20:02:51 +0100

    Update location text in About component

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -33,7 +33,7 @@ export const About = () => {
 
             <div className="flex items-center gap-2 text-gold font-montserrat">
               <MapPin className="w-5 h-5" />
-              <span className="text-lg">Las Rozas Village, Madrid</span>
+              <span className="text-lg">Las Rozas Village </span>
             </div>
           </motion.div>
 
